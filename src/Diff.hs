91309f3b7fd88447b74975a0cee719358c99256a
Viktor Kronvall <viktor.kronvall@gmail.com>
2021-08-28 04:14:58 +0900

    RAD Curry has arrived

--- src/Diff.hs
+++ src/Diff.hs
@@ -14,121 +14,32 @@
 module Diff where
 
 import Data.Monoid (Sum)
-import Data.Type.Equality (type (:~:) (Refl))
 
--- type Diff a b = Diff' a b (Tangent a) (Tangent b)
-
-{-
-vjp :: Diff a b -> a -> (Tangent b -> Tangent a)
-vjp (Diff f) x = snd (f x)
--}
-
--- Slow curry calling f for each gradient in the tangent space
-{-
-curryDiff ::
-  forall a b c ta tb tc.
-  Monoid ta =>
-  Diff' (a, b) c (ta, tb) tc ->
-  Diff' a (Diff' b c tb tc) ta [(b, tc)]
-curryDiff (Diff f) = Diff new_f
-  where
-    new_f a = (Diff new_g, h)
-      where
-        new_g :: b -> (c, tc -> tb)
-        new_g b = let (c, f') = f (a, b) in (c, snd . f')
-        h :: [(b, tc)] -> ta
-        h grads = foldl' (<>) mempty (map reach grads)
-          where
-            reach (b, grad) = fst (snd (f (a, b)) grad)
-
-curryDiff' ::
-  forall a b c ta tb tc.
-  Monoid ta =>
-  Diff' (a, b) c (ta, tb) tc ->
-  Diff' a (Diff' b c tb tc) ta [(b, tc)]
-curryDiff' (Diff f) = Diff new_f
-  where
-    new_f a = (Diff new_g, h)
-      where
-        new_g :: b -> (c, tc -> tb)
-        new_g b = let (c, f') = f (a, b) in (c, snd . f')
-        h :: [(b, tc)] -> ta
-        h grads = foldl' (<>) mempty (map reach grads)
-          where
-            reach (b, grad) = fst (snd (f (a, b)) grad)
--}
-
-{-
-curryDiff2 ::
-  forall a b c ta tb tc.
-  Monoid ta =>
-  Diff' (a, b) c (ta, tb) tc ->
-  Diff' a (Diff' b c tb tc) ta [(b, tc)]
-curryDiff2 (Diff f) = Diff new_f
-  where
-    new_f :: a -> (Diff' b c tb tc, [(b, tc)] -> ta)
-    new_f a = (Diff g, new_pb)
-      where
-        gf :: b -> (c, tc -> tb)
-        gf b =
-          let (c, pb) = f (a, b)
-           in ( c,
-                \tc ->
-                  let (cte, (ctt, cts)) = pb tc
-                   in ((cte, ctt), cts)
-              )
-        g :: b -> (c, tc -> tb)
-        g = gf
-
-        new_pb :: [(b, tc)] -> ta
-        new_pb env = env
-  -}
+-- Differentable function from a to b with some extra hidden state tx for the
+-- backprop
+type Arr tx a b =
+  ( forall ta.
+    Point a ta ->
+    ( forall r.
+      (forall tb. Point b tb -> (tb -> (tx, ta)) -> r) ->
+      r
+    )
+  )
 
 data a :-> b where
-  D ::
-    Tangent x tx ->
-    ( forall ta.
-      a ->
-      Tangent a ta ->
-      ( forall r.
-        (forall tb. Tangent b tb -> (b, tb -> (tx, ta)) -> r) ->
-        r
-      )
-    ) ->
-    a :-> b
+  D :: Tangent x tx -> Arr tx a b -> a :-> b
 
-class KnownTangent a where
-  type Tan a
-  knownTangent :: Tangent a (Tan a)
-  tangent :: Tangent a ta -> (ta ~ Tan a => r) -> r
+-- Correlate a point with its tangent
+-- This is needed for the Arrow case to ensure
+-- that the tangent tx is corresponding to the exponential (a :-> b)
+-- and not just existentially quantified inside (a :-> b)
+data Point a ta where
+  Arrow :: Tangent x tx -> Arr tx a b -> Point (a :-> b) tx
+  Point :: a -> Tangent a ta -> Point a ta
+  PointTuple :: Point a ta -> Point b tb -> Point (a, b) (ta, tb)
 
-instance KnownTangent (Sum Float) where
-  type Tan (Sum Float) = Sum Float
-  knownTangent = TanFloat
-  tangent TanFloat r = r
-
-instance KnownTangent Int where
-  type Tan Int = ()
-  knownTangent = TanInt
-  tangent TanInt r = r
-
-instance KnownTangent Bool where
-  type Tan Bool = ()
-  knownTangent = TanBool
-  tangent TanBool r = r
-
-instance KnownTangent () where
-  type Tan () = ()
-  knownTangent = TanUnit
-  tangent TanUnit r = r
-
-instance (KnownTangent a, KnownTangent b) => KnownTangent (a, b) where
-  type Tan (a, b) = (Tan a, Tan b)
-  knownTangent = TanTuple knownTangent knownTangent
-  tangent (TanTuple a b) r = tangent a $ tangent b r
-
+-- Can not contain arrows because then we need the slow curry implementation
 data Tangent a ta where
-  TanDiff :: Tangent x tx -> Tangent (a :-> b) tx
   TanFloat :: Tangent (Sum Float) (Sum Float)
   TanTuple ::
     Tangent a ta ->
@@ -140,271 +51,175 @@ data Tangent a ta where
 
 deriving instance Show (Tangent a ta)
 
-monoidTangent :: Tangent a ta -> (Monoid ta => r) -> r
-monoidTangent TanFloat r = r
-monoidTangent (TanDiff x) r = monoidTangent x r
-monoidTangent (TanTuple a b) r = monoidTangent a $ monoidTangent b r
-monoidTangent TanUnit r = r
-monoidTangent TanInt r = r
-monoidTangent TanBool r = r
-
-node :: (KnownTangent a, KnownTangent b) => (a -> (b, Tan b -> Tan a)) -> a :-> b
-node f = D TanUnit $ \a tta k ->
-  tangent tta $
-    let (b, f') = f a
-     in k knownTangent (b, \tb -> ((), f' tb))
-
-node' :: Tangent b tb -> (forall ta. Tangent a ta -> a -> (b, tb -> ta)) -> a :-> b
-node' ttb f = D TanUnit $ \a tta k ->
-  let (b, f') = f tta a
-   in k ttb (b, \tb -> ((), f' tb))
-
-curryD ::
-  forall a b c.
-  (a, b) :-> c ->
-  a :-> (b :-> c)
-curryD (D (x :: Tangent x tx) f) =
-  D x new_f
-  where
-    new_f ::
-      forall ta.
-      a ->
-      Tangent a ta ->
-      ( forall r.
-        (forall tb. Tangent (b :-> c) tb -> (b :-> c, tb -> (tx, ta)) -> r) ->
-        r
-      )
-    new_f a tta k = k (TanDiff (TanTuple x tta)) (D (TanTuple x tta) g, id)
-      where
-        g ::
-          forall tb.
-          b ->
-          Tangent b tb ->
-          ( forall r'.
-            (forall tc. Tangent c tc -> (c, tc -> ((tx, ta), tb)) -> r') ->
-            r'
-          )
-        g b ttb k' =
-          let l = f (a, b) (TanTuple tta ttb)
-           in l $ \ttc -> reassoc $ k' ttc
-          where
-            reassoc m (c, n) = m (c, \tc -> let (tx, (ta, tb)) = n tc in ((tx, ta), tb))
-
-uncurry :: (a -> (b -> c)) -> (a, b) -> c
-uncurry f (a, b) = f a b
-
-uncurryD :: (a :-> (b :-> c)) -> (a, b) :-> c
-uncurryD (D _ f) = D TanUnit $ \(a, b) (TanTuple tta ttb) k ->
-  let kf = f a tta
-   in kf $ \ttb' (D y g, f') ->
-        let kg = g b ttb
-         in kg $ \ttc' (c, g') ->
-              k
-                ttc'
-                ( c,
-                  \tc ->
-                    case equalTangents ttb' y of
-                      Just Refl ->
-                        let (ty, tb) = g' tc
-                            (_, ta) = f' ty
-                         in ((), (ta, tb))
-                      Nothing -> error $ "Internal error: " <> show y <> " does not match passed " <> show ttb'
-                )
-
--- It is very unfortunate this did not just work out
-applyD :: (a :-> b, a) :-> b
-applyD = D TanUnit $ \(D x f, a) (TanTuple (TanDiff ttf) tta) k ->
-  case equalTangents x ttf of
-    Just Refl ->
-      let kf = f a tta
-       in kf $ \ttb (b, f') ->
-            k ttb $
-              tuple b $ \tb ->
-                ((), f' tb)
-    Nothing -> error $ "Internal error: " <> show x <> " does not match passed " <> show ttf
-
-equalTangents :: Tangent x tx -> Tangent y ty -> Maybe (tx :~: ty)
-equalTangents TanFloat TanFloat = Just Refl
-equalTangents TanFloat _ = Nothing
-equalTangents (TanDiff x) y = equalTangents x y
-equalTangents x (TanDiff y) = equalTangents x y
-equalTangents (TanTuple a b) (TanTuple c d) = do
-  Refl <- equalTangents a c
-  Refl <- equalTangents b d
-  pure Refl
-equalTangents (TanTuple _ _) _ = Nothing
-equalTangents x y = do
-  Refl <- unitTangent x
-  Refl <- unitTangent y
-  pure Refl
-  where
-    unitTangent :: Tangent a ta -> Maybe (ta :~: ())
-    unitTangent TanFloat = Nothing
-    unitTangent (TanTuple _ _) = Nothing
-    unitTangent (TanDiff a) = unitTangent a
-    unitTangent TanUnit = Just Refl
-    unitTangent TanBool = Just Refl
-    unitTangent TanInt = Just Refl
+-- | Arrows are not points
+arrow :: Point (a :-> b) tx -> (forall x. Tangent x tx -> Arr tx a b -> r) -> r
+arrow (Point _ t) _ = case t of
+arrow (Arrow x f) k = k x f
 
 idD :: a :-> a
-idD = D TanUnit $ \a tta k -> k tta (a, tuple ())
+idD = D TanUnit $ \p k -> k p ((),)
 
 sequenceD ::
   (a :-> b) ->
   (b :-> c) ->
   (a :-> c)
-sequenceD (D txf f) (D txg g) = D (TanTuple txf txg) $ \a tta k ->
-  let kf = f a tta
-   in kf $ \ttb (b, f') ->
-        let kg = g b ttb
-         in kg $ \ttc (c, g') ->
-              k ttc $
-                tuple c $ \tc ->
-                  let (xg, tb) = g' tc
-                      (xf, ta) = f' tb
-                   in ((xf, xg), ta)
+sequenceD (D txf f) (D txg g) = D (TanTuple txf txg) $ \pa k ->
+  let kf = f pa
+   in kf $ \pb f' ->
+        let kg = g pb
+         in kg $ \pc g' ->
+              k pc $ \tc ->
+                let (xg, tb) = g' tc
+                    (xf, ta) = f' tb
+                 in ((xf, xg), ta)
 
 parallelD :: a :-> b -> c :-> d -> (a, c) :-> (b, d)
 parallelD (D ttx f) (D tty g) =
-  D (TanTuple ttx tty) $ \(a, c) (TanTuple tta ttc) k ->
-    let kf = f a tta
-        kg = g c ttc
-     in kf $ \ttb (b, f') ->
-          kg $ \ttd (d, g') ->
-            k (TanTuple ttb ttd) $
-              tuple (b, d) $ \(tb, td) ->
+  D (TanTuple ttx tty) $ \pac k ->
+    untuplePoint pac $ \(pa, pc) ->
+      let kf = f pa
+          kg = g pc
+       in kf $ \pb f' ->
+            kg $ \pd g' ->
+              k (PointTuple pb pd) $ \(tb, td) ->
                 let (xf, ta) = f' tb
                     (xg, tc) = g' td
                  in ((xf, xg), (ta, tc))
 
-tuple :: a -> b -> (a, b)
-tuple a b = (a, b)
+fstD :: (a, b) :-> a
+fstD = D TanUnit $ \pab k -> untuplePoint pab $ \(pa, pb) ->
+  pointTangent pb $ \tb -> monoidTangent tb $ k pa (\ta -> ((), (ta, mempty)))
 
-vjp' ::
-  a :-> b ->
-  Tangent a ta ->
-  a ->
-  ( forall r.
-    ( forall tb. Tangent b tb -> (tb -> ta) -> r
-    ) ->
-    r
-  )
-vjp' (D _ f) tta x g =
-  let k = f x tta
-   in k $ \ttb (_, f') ->
-        g ttb $ \tb -> snd $ f' tb
+sndD :: (a, b) :-> b
+sndD = D TanUnit $ \pab k -> untuplePoint pab $ \(pa, pb) ->
+  pointTangent pa $ \ta -> monoidTangent ta $ k pb (\tb -> ((), (mempty, tb)))
 
-vjp :: (KnownTangent a, KnownTangent b) => (a :-> b) -> a -> Tan b -> Tan a
-vjp d x = vjp' d knownTangent x $ \ttb g -> tangent ttb g
+dupD :: a :-> (a, a)
+dupD = D TanUnit $ \pa k ->
+  pointTangent pa $ \tta ->
+    monoidTangent tta $ k (PointTuple pa pa) (\(ta, tb) -> ((), ta <> tb))
 
--- id_a where a is determined by the Tangent witness
-
-{-
-
-parallelDiff :: Diff a b -> Diff c d -> Diff (a, c) (b, d)
-parallelDiff (Diff f) (Diff g) = Diff $ \(a, c) ->
-  let (b, f') = f a
-      (d, g') = g c
-   in ((b, d), bimap f' g')
-
-sequenceDiff :: Diff a b -> Diff b c -> Diff a c
-sequenceDiff (Diff f) (Diff g) = Diff $ \a ->
-  let (b, f') = f a
-      (c, g') = g b
-   in (c, f' . g')
--}
-
-{-
--}
-
-{-
-let (c, g') = _ f
- in ( c,
-      \tc ->
-        let (tx, (ta, tb)) = g'
-         in ((tx, ta), tb)
-    )
--}
-
-{-
--}
-
-{-
-curryDiff2 :: forall a b c. Diff2 (a, b) c -> Diff2 a (Diff b c)
-curryDiff2 (Diff2 (TanDiff tx tc) f) =
-  Diff2 (_ tx tc) _
+curryD ::
+  forall a b c.
+  (a, b) :-> c ->
+  a :-> (b :-> c)
+curryD (D (x :: Tangent x tx) f) = D x h
   where
-    new_f ::
-      Tangent' a ta ->
-      Tangent' b tb ->
-      Tangent' c tc ->
-      a ->
-      (Diff' b c tb tc, [(b, tc)] -> (x, ta))
-    new_f ta' tb' tc' a = (Diff gf, _)
+    -- Arr tx a (b :-> c)
+    -- But I need ta in scope for g to have a type signature
+    h ::
+      forall ta.
+      Point a ta ->
+      ( forall r.
+        (forall tb. Point (b :-> c) tb -> (tb -> (tx, ta)) -> r) ->
+        r
+      )
+    h pa k = pointTangent pa $ \tta -> k (Arrow (TanTuple x tta) g) id
       where
-        gf :: b -> (c, tc -> ((x, ta), tb))
-        gf = undefined
+        -- g also backprops ta in the hidden state
+        g :: Arr (tx, ta) b c
+        g pb k' =
+          let kf = f (PointTuple pa pb)
+           in kf $ \pc f' ->
+                k' pc $ \tc ->
+                  let (tx, (ta, tb)) = f' tc
+                   in ((tx, ta), tb)
 
-    g :: Differ b c (Tangent b) tc
-    g = error "not implemented"
+uncurryD :: (a :-> (b :-> c)) -> (a, b) :-> c
+uncurryD (D _ f) = D TanUnit $ \pab k ->
+  untuplePoint pab $ \(pa, pb) ->
+    let kf = f pa
+     in kf $ \arrg f' ->
+          arrow arrg $ \_ g ->
+            let kg = g pb
+             in kg $ \pc g' ->
+                  k pc $ \tc ->
+                    let (ty, tb) = g' tc
+                        (_, ta) = f' ty
+                     in ((), (ta, tb))
 
-    g' :: [(b, tc)] -> Tangent a
-    g' = error "not implemented"
-    -}
+applyD :: (a :-> b, a) :-> b
+applyD = D TanUnit $ \pfa k ->
+  untuplePoint pfa $ \(pf, pa) ->
+    arrow pf $ \_ f ->
+      let kf = f pa
+       in kf $ \b f' ->
+            k b (\tc -> ((), f' tc))
 
-{-
-     curry :: ((τt,τs)  τr) →(τt  (τs  τr))
-curry (exT τ[ f) = exT () new_f
-where
-new_f :: Π(t:τt). Σ(g : τs  τr). T[g] →(τ[, T[t])
-new_f t =
-let gf :: Π(s:τs).Σ(r:τr). T[r] →((τ[,T[t]), T[s])
-gf s = let (r, pullback) = f(t,s)
-in (r, λgr →
-let (cte,(ctt,cts)) = pullback gr
-in (( cte,ctt), cts))
-g = exT (τ[,T[t]) gf
-new_pb :: T[g] →(τ[,T[t])
-new_pb env = env
-in (g, new_pb
--}
+untuplePoint :: Point (a, c) t -> (forall ta tc. t ~ (ta, tc) => (Point a ta, Point c tc) -> r) -> r
+untuplePoint (PointTuple a b) r = r (a, b)
+untuplePoint (Point (a, b) (TanTuple ta tb)) r = r (Point a ta, Point b tb)
 
-{-
-uncurryDiff :: forall a b c. Diff a (Diff b c) -> Diff (a, b) c
-uncurryDiff f = sequenceDiff (parallelDiff f idDiff) applyDiff
+-- Simplify converting simple functions (a -> b) to (a :-> b)
+class KnownTangent a where
+  type Tan a
+  knownTangent :: Tangent a (Tan a)
+  tangent :: Tangent a ta -> (ta ~ Tan a => r) -> r
+  pointTangent' :: Point a ta -> Tangent a ta
 
-idDiff :: Diff a a
-idDiff = Diff (,id)
+instance KnownTangent (Sum Float) where
+  type Tan (Sum Float) = Sum Float
+  knownTangent = TanFloat
+  tangent TanFloat r = r
+  pointTangent' (Point _ ta) = ta
 
-applyDiff :: Diff (Diff a b, a) b
-applyDiff = Diff $ \(Diff f, x) ->
-  let (b, f') = f x
-   in (b, \db -> ([(x, db)], f' db))
+instance KnownTangent Int where
+  type Tan Int = ()
+  knownTangent = TanInt
+  tangent TanInt r = r
+  pointTangent' (Point _ ta) = ta
 
-parallelDiff :: Diff a b -> Diff c d -> Diff (a, c) (b, d)
-parallelDiff (Diff f) (Diff g) = Diff $ \(a, c) ->
-  let (b, f') = f a
-      (d, g') = g c
-   in ((b, d), bimap f' g')
+instance KnownTangent Bool where
+  type Tan Bool = ()
+  knownTangent = TanBool
+  tangent TanBool r = r
+  pointTangent' (Point _ ta) = ta
 
-sequenceDiff :: Diff a b -> Diff b c -> Diff a c
-sequenceDiff (Diff f) (Diff g) = Diff $ \a ->
-  let (b, f') = f a
-      (c, g') = g b
-   in (c, f' . g')
+instance KnownTangent () where
+  type Tan () = ()
+  knownTangent = TanUnit
+  tangent TanUnit r = r
+  pointTangent' (Point _ ta) = ta
 
-multiply :: Diff (Float, Float) Float
-multiply = Diff $ \(a, b) -> (a * b, \dc -> (dc * b, dc * a))
+instance (KnownTangent a, KnownTangent b) => KnownTangent (a, b) where
+  type Tan (a, b) = (Tan a, Tan b)
+  knownTangent = TanTuple knownTangent knownTangent
+  tangent (TanTuple a b) r = tangent a $ tangent b r
+  pointTangent' (Point _ ta) = ta
+  pointTangent' (PointTuple pa pb) = TanTuple (pointTangent' pa) (pointTangent' pb)
 
-add :: Monoid a => Diff' (a, a) a (ta, ta) ta
-add = Diff $ \(a, b) -> (a <> b, \dc -> (dc, dc))
+node :: forall a b. (KnownTangent a, KnownTangent b) => (a -> (b, Tan b -> Tan a)) -> a :-> b
+node f = D TanUnit $ \(pa :: Point a ta) k ->
+  tangent (pointTangent' pa) $
+    let (b, f') = f (pointValue pa)
+     in k (Point b knownTangent) (\tb -> ((), f' tb))
 
-fstDiff :: Monoid tb => Diff' (a, b) a (ta, tb) ta
-fstDiff = Diff $ \(a, _) -> (a, (,mempty))
+multiply :: (Sum Float, Sum Float) :-> Sum Float
+multiply = node $ \(a, b) -> (a * b, \tc -> (tc * b, tc * a))
 
-sndDiff :: Monoid ta => Diff' (a, b) b (ta, tb) tb
-sndDiff = Diff $ \(_, b) -> (b, (mempty,))
+add :: (Sum Float, Sum Float) :-> Sum Float
+add = node $ \(a, b) -> (a <> b, \tc -> (tc, tc))
 
-dupDiff :: Monoid ta => Diff' a (a, a) ta (ta, ta)
-dupDiff = Diff $ \a -> ((a, a), uncurry (<>))
--}
+equalTangent :: Tangent a ta -> Tangent a tx -> (ta ~ tx => r) -> r
+equalTangent TanFloat TanFloat r = r
+equalTangent (TanTuple a b) (TanTuple c d) r = equalTangent a c $ equalTangent b d r
+equalTangent TanUnit TanUnit r = r
+equalTangent TanInt TanInt r = r
+equalTangent TanBool TanBool r = r
+
+monoidTangent :: Tangent a ta -> (Monoid ta => r) -> r
+monoidTangent TanFloat r = r
+monoidTangent (TanTuple a b) r = monoidTangent a $ monoidTangent b r
+monoidTangent TanUnit r = r
+monoidTangent TanInt r = r
+monoidTangent TanBool r = r
+
+pointTangent :: Point a ta -> (forall b. Tangent b ta -> r) -> r
+pointTangent (Point _ ta) k = k ta
+pointTangent (Arrow ta _) k = k ta
+pointTangent (PointTuple pa pb) k = pointTangent pa $ \ta -> pointTangent pb $ \tb -> k (TanTuple ta tb)
+
+pointValue :: Point a ta -> a
+pointValue (Point a _) = a
+pointValue (Arrow x f) = D x f
+pointValue (PointTuple a b) = (pointValue a, pointValue b)
