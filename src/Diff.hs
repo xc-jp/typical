908141b7e85aded56621c59f2e0a4838736f6d4d
Viktor Kronvall <viktor.kronvall@gmail.com>
2021-08-30 13:52:21 +0900

    Lists and projections

--- src/Diff.hs
+++ src/Diff.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
@@ -26,38 +27,116 @@ type Arr tx a b =
     )
   )
 
+-- | Types of valid tangents
+data Tan a where
+  TanFloat :: Tan (Sum Float)
+  TanTuple :: Tan a -> Tan b -> Tan (a, b)
+  TanUnit :: Tan ()
+  TanNil :: Tan (List '[])
+  TanCons :: Tan x -> Tan (List xs) -> Tan (List (x ': xs))
+
+deriving instance Show (Tan ta)
+
 data a :-> b where
-  D :: Tangent x tx -> Arr tx a b -> a :-> b
+  D :: Tan tx -> Arr tx a b -> a :-> b
 
--- Correlate a point with its tangent
--- This is needed for the Arrow case to ensure
--- that the tangent tx is corresponding to the exponential (a :-> b)
--- and not just existentially quantified inside (a :-> b)
 data Point a ta where
-  Arrow :: Tangent x tx -> Arr tx a b -> Point (a :-> b) tx
-  Point :: a -> Tangent a ta -> Point a ta
+  Arrow :: Tan tx -> Arr tx a b -> Point (a :-> b) tx
+  PointFloat :: Sum Float -> Point (Sum Float) (Sum Float)
   PointTuple :: Point a ta -> Point b tb -> Point (a, b) (ta, tb)
+  PointUnit :: Point () ()
+  PointInt :: Int -> Point Int ()
+  PointBool :: Bool -> Point Bool ()
+  PointNil :: Point (List '[]) (List '[])
+  PointCons :: Point x t -> Point (List xs) (List ts) -> Point (List (x ': xs)) (List (t ': ts))
+  PointIZ :: Point (Index (x ': xs) x) ()
+  PointIS :: Point (Index xs x) () -> Point (Index (y ': xs) x) ()
 
--- Can not contain arrows because then we need the slow curry implementation
-data Tangent a ta where
-  TanFloat :: Tangent (Sum Float) (Sum Float)
-  TanTuple ::
-    Tangent a ta ->
-    Tangent b tb ->
-    Tangent (a, b) (ta, tb)
-  TanUnit :: Tangent () ()
-  TanInt :: Tangent Int ()
-  TanBool :: Tangent Bool ()
+instance Show (Point a ta) where
+  show (Arrow t _) = "<<arr>> : " <> show t
+  show (PointFloat x) = show x
+  show (PointTuple a b) = show (a, b)
+  show PointUnit = "unit"
+  show (PointInt x) = show x
+  show (PointBool x) = show x
+  show PointNil = "[]"
+  show (PointCons x xs) = show x <> " :: " <> show xs
+  show PointIZ = "[0]"
+  show (PointIS i) = "[" <> show (succ (ix i)) <> "]"
+    where
+      ix :: Point (Index xs x) () -> Int
+      ix PointIZ = 0
+      ix (PointIS n) = succ (ix n)
 
-deriving instance Show (Tangent a ta)
+monoidTangent :: Point a ta -> (Monoid ta => r) -> r
+monoidTangent (Arrow x _) r = monoidTan x r
+monoidTangent (PointFloat _) r = r
+monoidTangent (PointTuple a b) r = monoidTangent a $ monoidTangent b r
+monoidTangent PointUnit r = r
+monoidTangent (PointInt _) r = r
+monoidTangent (PointBool _) r = r
+monoidTangent PointNil r = r
+monoidTangent (PointCons t ts) r = monoidTangent t $ monoidTangent ts r
+monoidTangent PointIZ r = r
+monoidTangent (PointIS _) r = r
 
--- | Arrows are not points
-arrow :: Point (a :-> b) tx -> (forall x. Tangent x tx -> Arr tx a b -> r) -> r
-arrow (Point _ t) _ = case t of
-arrow (Arrow x f) k = k x f
+monoidTan :: Tan a -> (Monoid a => r) -> r
+monoidTan TanFloat r = r
+monoidTan (TanTuple a b) r = monoidTan a $ monoidTan b r
+monoidTan TanUnit r = r
+monoidTan TanNil r = r
+monoidTan (TanCons x xs) r = monoidTan x $ monoidTan xs r
+
+pointTangent :: Point a ta -> Tan ta
+pointTangent (Arrow x _) = x
+pointTangent (PointTuple a b) = TanTuple (pointTangent a) (pointTangent b)
+pointTangent PointFloat {} = TanFloat
+pointTangent PointUnit {} = TanUnit
+pointTangent PointInt {} = TanUnit
+pointTangent PointBool {} = TanUnit
+pointTangent PointNil {} = TanNil
+pointTangent (PointCons x xs) = TanCons (pointTangent x) (pointTangent xs)
+pointTangent PointIZ = TanUnit
+pointTangent PointIS {} = TanUnit
+
+data Index xs t where
+  IZ :: Index (t ': xs) t
+  IS :: Index xs t -> Index (y ': xs) t
+
+data List xs where
+  Nil :: List '[]
+  Cons :: x -> List xs -> List (x ': xs)
+
+instance Semigroup (List '[]) where
+  _ <> _ = Nil
+
+instance (Semigroup t, Semigroup (List ts)) => Semigroup (List (t ': ts)) where
+  Cons x xs <> Cons y ys = Cons (x <> y) (xs <> ys)
+
+instance Monoid (List '[]) where
+  mempty = Nil
+
+instance (Monoid t, Monoid (List ts)) => Monoid (List (t ': ts)) where
+  mempty = Cons mempty mempty
 
 idD :: a :-> a
 idD = D TanUnit $ \p k -> k p ((),)
+
+constD :: Point b tb -> (a :-> b)
+constD b = D TanUnit $ \pa k ->
+  monoidTangent pa $
+    k b (const ((), mempty))
+
+atD :: Index xs x -> List xs :-> x
+atD i = D TanUnit $ \xs k -> at i xs $ \j p -> k p (\t -> ((), backpropAt j xs t))
+
+at :: Index xs x -> Point (List xs) tl -> (forall ts t. (tl ~ List ts) => Index ts t -> Point x t -> r) -> r
+at IZ (PointCons t _) k = k IZ t
+at (IS i) (PointCons _ xs) k = at i xs $ \j t -> k (IS j) t
+
+backpropAt :: Index ts t -> Point (List xs) (List ts) -> t -> List ts
+backpropAt IZ (PointCons _ tail') t = monoidTangent tail' $ Cons t mempty
+backpropAt (IS i) (PointCons head' ts) t = monoidTangent head' $ Cons mempty (backpropAt i ts t)
 
 sequenceD ::
   (a :-> b) ->
@@ -75,35 +154,30 @@ sequenceD (D txf f) (D txg g) = D (TanTuple txf txg) $ \pa k ->
 
 parallelD :: a :-> b -> c :-> d -> (a, c) :-> (b, d)
 parallelD (D ttx f) (D tty g) =
-  D (TanTuple ttx tty) $ \pac k ->
-    untuplePoint pac $ \(pa, pc) ->
-      let kf = f pa
-          kg = g pc
-       in kf $ \pb f' ->
-            kg $ \pd g' ->
-              k (PointTuple pb pd) $ \(tb, td) ->
-                let (xf, ta) = f' tb
-                    (xg, tc) = g' td
-                 in ((xf, xg), (ta, tc))
+  D (TanTuple ttx tty) $ \(PointTuple pa pc) k ->
+    let kf = f pa
+        kg = g pc
+     in kf $ \pb f' ->
+          kg $ \pd g' ->
+            k (PointTuple pb pd) $ \(tb, td) ->
+              let (xf, ta) = f' tb
+                  (xg, tc) = g' td
+               in ((xf, xg), (ta, tc))
 
 fstD :: (a, b) :-> a
-fstD = D TanUnit $ \pab k -> untuplePoint pab $ \(pa, pb) ->
-  pointTangent pb $ \tb -> monoidTangent tb $ k pa (\ta -> ((), (ta, mempty)))
+fstD = D TanUnit $ \(PointTuple pa pb) k -> monoidTangent pb $ k pa (\ta -> ((), (ta, mempty)))
 
 sndD :: (a, b) :-> b
-sndD = D TanUnit $ \pab k -> untuplePoint pab $ \(pa, pb) ->
-  pointTangent pa $ \ta -> monoidTangent ta $ k pb (\tb -> ((), (mempty, tb)))
+sndD = D TanUnit $ \(PointTuple pa pb) k -> monoidTangent pa $ k pb (\tb -> ((), (mempty, tb)))
 
 dupD :: a :-> (a, a)
-dupD = D TanUnit $ \pa k ->
-  pointTangent pa $ \tta ->
-    monoidTangent tta $ k (PointTuple pa pa) (\(ta, tb) -> ((), ta <> tb))
+dupD = D TanUnit $ \pa k -> monoidTangent pa $ k (PointTuple pa pa) (\(ta, tb) -> ((), ta <> tb))
 
 curryD ::
   forall a b c.
   (a, b) :-> c ->
   a :-> (b :-> c)
-curryD (D (x :: Tangent x tx) f) = D x h
+curryD (D (x :: Tan tx) f) = D x h
   where
     -- Arr tx a (b :-> c)
     -- But I need ta in scope for g to have a type signature
@@ -114,7 +188,7 @@ curryD (D (x :: Tangent x tx) f) = D x h
         (forall tb. Point (b :-> c) tb -> (tb -> (tx, ta)) -> r) ->
         r
       )
-    h pa k = pointTangent pa $ \tta -> k (Arrow (TanTuple x tta) g) id
+    h pa k = k (Arrow (TanTuple x (pointTangent pa)) g) id
       where
         -- g also backprops ta in the hidden state
         g :: Arr (tx, ta) b c
@@ -126,100 +200,73 @@ curryD (D (x :: Tangent x tx) f) = D x h
                    in ((tx, ta), tb)
 
 uncurryD :: (a :-> (b :-> c)) -> (a, b) :-> c
-uncurryD (D _ f) = D TanUnit $ \pab k ->
-  untuplePoint pab $ \(pa, pb) ->
-    let kf = f pa
-     in kf $ \arrg f' ->
-          arrow arrg $ \_ g ->
-            let kg = g pb
-             in kg $ \pc g' ->
-                  k pc $ \tc ->
-                    let (ty, tb) = g' tc
-                        (_, ta) = f' ty
-                     in ((), (ta, tb))
+uncurryD (D _ f) = D TanUnit $ \(PointTuple pa pb) k ->
+  let kf = f pa
+   in kf $ \(Arrow _ g) f' ->
+        let kg = g pb
+         in kg $ \pc g' ->
+              k pc $ \tc ->
+                let (ty, tb) = g' tc
+                    (_, ta) = f' ty
+                 in ((), (ta, tb))
 
 applyD :: (a :-> b, a) :-> b
-applyD = D TanUnit $ \pfa k ->
-  untuplePoint pfa $ \(pf, pa) ->
-    arrow pf $ \_ f ->
-      let kf = f pa
-       in kf $ \b f' ->
-            k b (\tc -> ((), f' tc))
+applyD = D TanUnit $ \(PointTuple (Arrow _ f) pa) k ->
+  let kf = f pa
+   in kf $ \b f' ->
+        k b (\tc -> ((), f' tc))
 
-untuplePoint :: Point (a, c) t -> (forall ta tc. t ~ (ta, tc) => (Point a ta, Point c tc) -> r) -> r
-untuplePoint (PointTuple a b) r = r (a, b)
-untuplePoint (Point (a, b) (TanTuple ta tb)) r = r (Point a ta, Point b tb)
+-- | Simplify construction of primitive functions
+class KnownPoint a where
+  type Tangent a
+  knownPoint :: a -> Point a (Tangent a)
+  uniquePoint :: Point a ta -> (ta ~ Tangent a => r) -> r
 
--- Simplify converting simple functions (a -> b) to (a :-> b)
-class KnownTangent a where
-  type Tan a
-  knownTangent :: Tangent a (Tan a)
-  tangent :: Tangent a ta -> (ta ~ Tan a => r) -> r
-  pointTangent' :: Point a ta -> Tangent a ta
+instance KnownPoint (Sum Float) where
+  type Tangent (Sum Float) = Sum Float
+  knownPoint = PointFloat
+  uniquePoint (PointFloat _) r = r
 
-instance KnownTangent (Sum Float) where
-  type Tan (Sum Float) = Sum Float
-  knownTangent = TanFloat
-  tangent TanFloat r = r
-  pointTangent' (Point _ ta) = ta
+instance KnownPoint Int where
+  type Tangent Int = ()
+  knownPoint = PointInt
+  uniquePoint (PointInt _) r = r
 
-instance KnownTangent Int where
-  type Tan Int = ()
-  knownTangent = TanInt
-  tangent TanInt r = r
-  pointTangent' (Point _ ta) = ta
+instance KnownPoint Bool where
+  type Tangent Bool = ()
+  knownPoint = PointBool
+  uniquePoint (PointBool _) r = r
 
-instance KnownTangent Bool where
-  type Tan Bool = ()
-  knownTangent = TanBool
-  tangent TanBool r = r
-  pointTangent' (Point _ ta) = ta
+instance KnownPoint () where
+  type Tangent () = ()
+  knownPoint _ = PointUnit
+  uniquePoint PointUnit r = r
 
-instance KnownTangent () where
-  type Tan () = ()
-  knownTangent = TanUnit
-  tangent TanUnit r = r
-  pointTangent' (Point _ ta) = ta
+instance (KnownPoint a, KnownPoint b) => KnownPoint (a, b) where
+  type Tangent (a, b) = (Tangent a, Tangent b)
+  knownPoint (a, b) = PointTuple (knownPoint a) (knownPoint b)
+  uniquePoint (PointTuple a b) r = uniquePoint a $ uniquePoint b r
 
-instance (KnownTangent a, KnownTangent b) => KnownTangent (a, b) where
-  type Tan (a, b) = (Tan a, Tan b)
-  knownTangent = TanTuple knownTangent knownTangent
-  tangent (TanTuple a b) r = tangent a $ tangent b r
-  pointTangent' (Point _ ta) = ta
-  pointTangent' (PointTuple pa pb) = TanTuple (pointTangent' pa) (pointTangent' pb)
-
-node :: forall a b. (KnownTangent a, KnownTangent b) => (a -> (b, Tan b -> Tan a)) -> a :-> b
-node f = D TanUnit $ \(pa :: Point a ta) k ->
-  tangent (pointTangent' pa) $
+prim :: forall a b. (KnownPoint a, KnownPoint b) => (a -> (b, Tangent b -> Tangent a)) -> a :-> b
+prim f = D TanUnit $ \pa k ->
+  uniquePoint pa $
     let (b, f') = f (pointValue pa)
-     in k (Point b knownTangent) (\tb -> ((), f' tb))
+     in k (knownPoint b) (\tb -> ((), f' tb))
 
 multiply :: (Sum Float, Sum Float) :-> Sum Float
-multiply = node $ \(a, b) -> (a * b, \tc -> (tc * b, tc * a))
+multiply = prim $ \(a, b) -> (a * b, \tc -> (tc * b, tc * a))
 
 add :: (Sum Float, Sum Float) :-> Sum Float
-add = node $ \(a, b) -> (a <> b, \tc -> (tc, tc))
-
-equalTangent :: Tangent a ta -> Tangent a tx -> (ta ~ tx => r) -> r
-equalTangent TanFloat TanFloat r = r
-equalTangent (TanTuple a b) (TanTuple c d) r = equalTangent a c $ equalTangent b d r
-equalTangent TanUnit TanUnit r = r
-equalTangent TanInt TanInt r = r
-equalTangent TanBool TanBool r = r
-
-monoidTangent :: Tangent a ta -> (Monoid ta => r) -> r
-monoidTangent TanFloat r = r
-monoidTangent (TanTuple a b) r = monoidTangent a $ monoidTangent b r
-monoidTangent TanUnit r = r
-monoidTangent TanInt r = r
-monoidTangent TanBool r = r
-
-pointTangent :: Point a ta -> (forall b. Tangent b ta -> r) -> r
-pointTangent (Point _ ta) k = k ta
-pointTangent (Arrow ta _) k = k ta
-pointTangent (PointTuple pa pb) k = pointTangent pa $ \ta -> pointTangent pb $ \tb -> k (TanTuple ta tb)
+add = prim $ \(a, b) -> (a <> b, \tc -> (tc, tc))
 
 pointValue :: Point a ta -> a
-pointValue (Point a _) = a
 pointValue (Arrow x f) = D x f
 pointValue (PointTuple a b) = (pointValue a, pointValue b)
+pointValue PointIZ = IZ
+pointValue (PointIS p) = IS (pointValue p)
+pointValue (PointFloat x) = x
+pointValue (PointInt x) = x
+pointValue (PointBool x) = x
+pointValue PointUnit = ()
+pointValue PointNil = Nil
+pointValue (PointCons x xs) = Cons (pointValue x) (pointValue xs)
